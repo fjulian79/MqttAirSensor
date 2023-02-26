a11f3323741e8430b6ce1d2caa4ff8faf49a0280
Julian Friedrich <julian.friedrich@gmx.at>
2023-02-26 21:51:39 +0100

    Added GPL v3 Licence

--- lib/mqttclient/mqttclient.hpp
+++ lib/mqttclient/mqttclient.hpp
@@ -20,6 +20,28 @@ typedef struct
 
 }MqttClientCfg_t;
 
+/*
+ * MqttAirSensor, yet another ESP32 based Sensor which transmits air 
+ * temperature and humidity to a MQTT broker. Why? Because I can!
+ *
+ * Copyright (C) 2023 Julian Friedrich
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>. 
+ *
+ * You can file issues at https://github.com/fjulian79/MqttAirSensor/issues
+ */
+
 class MqttClient
 {
     public:
